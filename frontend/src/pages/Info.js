c47694a73a3313f200331f26cc9e48014ef3b309
SaturnAnton <antongiuliodonno1@gmail.com>
2025-10-11 00:22:39 +0200

    Modifica della parte grafica

--- frontend/src/pages/Info.js
+++ frontend/src/pages/Info.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Info = () => (
   <div className="page-container">
-    <h1>ℹ️ Informazioni sul progetto</h1>
+    <h1>Informazioni sul progetto</h1>
     <p>
       Fantacalcio AI è un'applicazione per gestire la tua squadra di Serie A e
       prevedere le performance dei giocatori usando dati reali e predizioni AI.
